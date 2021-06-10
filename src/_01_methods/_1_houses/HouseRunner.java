698c605312fff9382119a1fc266e7b8642aea6a3
championgirl <66965981+championgirl@users.noreply.github.com>
2021-06-10 16:57:38 -0700

    HouseRunner

---
+++ src/_01_methods/_1_houses/HouseRunner.java
@@ -0,0 +1,10 @@
+package _01_methods._1_houses;
+
+public class HouseRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+new Houses().run();
+	}
+
+}
--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,11 +1,45 @@
 package _01_methods._1_houses;
+
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
 public class Houses {
-		
+	Robot coder = new Robot();
 	public void run() {
+		
 		// Check the recipe to find out what code to put here
+		
+	   
+	   	coder.setX(50);
+     coder.setY(500);
+     coder.setSpeed(1000);
+     coder.penDown();
+     for(int i = 0; i<=10; i++) {
+     houseDrawer();
+     }
+     coder.hide();
+     
+     
 	}
+	
+	void houseDrawer(int height) {
+	coder.setAngle(0);
+		coder.setRandomPenColor();
+	     coder.move(100);
+	     coder.turn(90);
+	     coder.move(50);
+	     coder.turn(90);
+	     coder.move(100);
+	     coder.setPenColor(Color.green);
+	     coder.turn(-90);
+	     coder.move(50);
+		
+	}
+	
 }
+
