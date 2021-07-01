e5772bb9cdbde8fa587953b87801b6927580e15a
championgirl <66965981+championgirl@users.noreply.github.com>
2021-07-01 16:52:54 -0700

    HouseDrawer

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,6 +1,7 @@
 package _01_methods._1_houses;
 
 import java.awt.Color;
+import java.util.Random;
 
 import org.jointheleague.graphical.robot.Robot;
 
@@ -19,39 +20,79 @@ public class Houses {
      coder.setY(500);
      coder.setSpeed(1000);
      coder.penDown();
+     
      for(int i = 0; i<=8; i++) {
-     houseDrawer(i*50+100);
+    	 
+  Random ran = new Random();
+     int number = ran.nextInt(3) ;
+     if (number==0) {
+     houseDrawer("small");
      }
+     else if (number==1) {
+         houseDrawer("medium");
+         }
+     else if(number ==2) {
+    	 houseDrawer("large");
+     }
+     }
+    
      coder.hide();
      
      
 	}
 	void houseDrawer(String height) {
 		if(height.equalsIgnoreCase("small")) {
-			houseDrawer(60);
+			houseDrawer(60,Color.RED);
 		}
 		else if(height.equalsIgnoreCase("medium")) {
-			houseDrawer(120);
+			houseDrawer(120,Color.BLUE);
+			
 		}
 		
-		else if(height.equalsIgnoreCase("Large")) {
-			houseDrawer(250);
+		else if(height.equalsIgnoreCase("large")) {
+			houseDrawer(250,Color.YELLOW);
 		}
 		
 	}
+	
+
+	
 	void houseDrawer(int height) {
 		
-	coder.setAngle(0);
-		coder.setRandomPenColor();
+	     coder.setAngle(0);
 	     coder.move(height);
-	     coder.turn(90);
-	     coder.move(50);
-	     coder.turn(90);
+	     if(height>200) {
+	 		drawFlatRoof();
+	 	}
+	     else {
+	     drawPointyRoof();}
+	     
 	     coder.move(height);
 	     coder.setPenColor(Color.green);
 	     coder.turn(-90);
 	     coder.move(50);
 		
+	}
+	
+	void drawFlatRoof() {
+		  coder.turn(90);
+		     coder.move(50);
+		     coder.turn(90);
+	}
+	
+	void drawPointyRoof() {
+		     coder.turn(45);
+		     coder.move(30);
+		     coder.turn(90);
+		     coder.move(30);
+		     coder.turn(45);
+	}
+	
+	
+	
+	void houseDrawer(int height,Color color) {
+		coder.setPenColor(color);
+		houseDrawer(height);
 	}
 	
 }
