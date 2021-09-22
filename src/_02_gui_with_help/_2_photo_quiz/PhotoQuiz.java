ee4e590f881fed4ba3ef966e9e0278a5349e1839
championgirl <66965981+championgirl@users.noreply.github.com>
2021-09-21 18:26:37 -0700

    photoquizDONE, DrumKitWIIP

--- src/_02_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_02_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -25,33 +25,200 @@ public class PhotoQuiz {
 
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
-		// “Copy Image Address” )
-
+		// â€œCopy Image Addressâ€� )
+String image = "https://static.wikia.nocookie.net/tangledtheseries/images/6/6d/Varian_Vertical.jpg/revision/latest/scale-to-width-down/376?cb=20180122041916";
 		// 2. create a variable of type "Component" that will hold your image
-
+ Component holder;
 		// 3. use the "createImage()" method below to initialize your Component
-
+ holder = createImage(image);
 		// 4. add the image to the quiz window
-
+quizWindow.add(holder);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
+String QnA = JOptionPane.showInputDialog("What is the charector's name?");
 
-		// 7. print "CORRECT" if the user gave the right answer
+// 7. print "CORRECT" if the user gave the right answer
 
+if(QnA.equalsIgnoreCase("Varian")) {
+	
+System.out.println("CORRECT");
+
+}
+
+else {
+	System.out.println("INCORRECT");
+}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizWindow.remove(holder);
 		// 10. find another image and create it (might take more than one line
 		// of code)
+ image = "http://vignette2.wikia.nocookie.net/p__/images/6/67/Tangled_-_Cassandra.png/revision/latest?cb=20170326175157&path-prefix=protagonist";
 
+// 3. use the "createImage()" method below to initialize your Component
+holder = createImage(image);
+// 4. add the image to the quiz window
+quizWindow.add(holder);
+// 5. call the pack() method on the quiz window
+quizWindow.pack();
 		// 11. add the second image to the quiz window
 
 		// 12. pack the quiz window
 
 		// 13. ask another question
+QnA = JOptionPane.showInputDialog("What is the charector's name?");
+
+
+if(QnA.equalsIgnoreCase("Cassandra")) {
+	
+System.out.println("CORRECT");
+
+}
+
+if(QnA.equalsIgnoreCase("Cass")) {
+	
+System.out.println("CORRECT");
+
+}
+
+if(QnA.equalsIgnoreCase("Cassie")) {
+	
+System.out.println("CORRECT");
+
+}
+
+else {
+	System.out.println("INCORRECT");
+}
+/////////////////////////////////////////////////////////////////////////////
+quizWindow.remove(holder);
+// 10. find another image and create it (might take more than one line
+// of code)
+image = "https://i.pinimg.com/736x/4b/2b/7a/4b2b7ae691043d882ee365cb521a4608.jpg";
+
+
+holder = createImage(image);
+
+quizWindow.add(holder);
+
+quizWindow.pack();
+
+
+QnA = JOptionPane.showInputDialog("How is Cassandra getting her power?");
+
+
+if(QnA.equalsIgnoreCase("The moonstone")) {
+	
+System.out.println("CORRECT");
+
+}
+
+if(QnA.equalsIgnoreCase("moonstone")) {
+	
+System.out.println("CORRECT");
+
+}
+
+if(QnA.equalsIgnoreCase("She is getting her power from the moonstone")) {
+	
+System.out.println("CORRECT");
+
+}
+
+else {
+	System.out.println("INCORRECT");
+}
+////////////////////////////////////////////////////////////////
+
+quizWindow.remove(holder);
+//10. find another image and create it (might take more than one line
+//of code)
+image = "https://static.wikia.nocookie.net/tangledtheseries/images/a/ad/Cass2.png/revision/latest/scale-to-width-down/446?cb=20200415113402";
+
+
+holder = createImage(image);
+
+quizWindow.add(holder);
+
+quizWindow.pack();
+
+
+QnA = JOptionPane.showInputDialog("Is Cass in this picture? ; Are these to women related? (Put a ; to seperate answers)");
+
+
+if(QnA.equalsIgnoreCase("yes;yes")) {
+	
+System.out.println("CORRECT");
+
+}
+
+if(QnA.equalsIgnoreCase("Duh;Bruh")) {
+	
+System.out.println("CORRECT");
+
+}
+
+if(QnA.equalsIgnoreCase("YASS,sadlyYASS")) {
+	
+System.out.println("CORRECT");
+
+}
+
+if(QnA.equalsIgnoreCase("yes,maybe")) {
+	
+System.out.println("CORRECT");
+
+}
+
+else {
+	System.out.println("INCORRECT");
+	
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+quizWindow.remove(holder);
+//10. find another image and create it (might take more than one line
+//of code)
+image = "https://i.pinimg.com/474x/72/bd/24/72bd240dbe99e80d6badd66cf0cc0972.jpg";
+
+
+holder = createImage(image);
+
+quizWindow.add(holder);
+
+quizWindow.pack();
+
+
+QnA = JOptionPane.showInputDialog("What song is Varian singing, put spaces in ur answer, C yall this is a question abt Varian not cass in 4eva ha xD )");
+
+
+if(QnA.equalsIgnoreCase("let me make you proud")) {
+	
+System.out.println("CORRECT");
+
+}
+
+if(QnA.equalsIgnoreCase("I will make you proud")) {
+	
+System.out.println("CORRECT");
+
+}
+
+if(QnA.equalsIgnoreCase("Some proud song lol")) {
+	
+System.out.println("CORRECT");
+
+}
+
+else {
+	System.out.println("INCORRECT");
+	
+}
+
 
 		// 14+ check answer, say if correct or incorrect, etc.
 
