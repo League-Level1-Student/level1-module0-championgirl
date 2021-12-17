23697375b0c3ec95d907a9e0526e6326125f513d
championgirl <66965981+championgirl@users.noreply.github.com>
2021-12-16 18:23:07 -0800

    FortuneCookieWIP

--- src/_03_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
+++ src/_03_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
@@ -1,5 +1,49 @@
 package _03_gui_from_scratch._1_fortune_cookie;
 
 public class FortuneCookie {
-
+	
+	 public void showButton() {
+         System.out.println("Button clicked");
+    }
+	 
+	 new FortuneCookie().showButton(frame.setVisible(true));
 }
+
+JButton button = new JButton();
+frame.add(button);
+frame.pack();
+
+JOptionPane.showMessageDialog(null,"WooHoo");
+
+button.addActionListener(this);
+
+	 
+int rand = new Random().nextInt(5);
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+}
+
