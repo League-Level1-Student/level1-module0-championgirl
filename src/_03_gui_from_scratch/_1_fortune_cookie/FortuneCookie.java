08e08c1aa4634958c5a9a516b6ffc0883626bf0a
championgirl <66965981+championgirl@users.noreply.github.com>
2022-01-10 18:28:30 -0800

    FortuneCookieDONE!!!JACKINTHEBOXWIP!

--- src/_03_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
+++ src/_03_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
@@ -1,13 +1,15 @@
 package _03_gui_from_scratch._1_fortune_cookie;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-public class FortuneCookie {
-	
+public class FortuneCookie implements ActionListener{
+			Random ran = new Random();
 	 public void showButton() {
          System.out.println("Button clicked");
          JFrame frame = new JFrame("The Fortune Cookie Program");
@@ -16,17 +18,35 @@ public class FortuneCookie {
          JButton button = new JButton("Click for a fortune");
          frame.add(button);
      
-        button.addActionListener((ActionListener) this);
+        button.addActionListener(this);
          frame.pack();
          JOptionPane.showMessageDialog(null, "Woohoo");
 	 }
 
-	 actionPerformed(){
-         JOptionPane.showMessageDialog(null, "Woohoo");
 
-         int rand = new Random().nextInt(5);
+         
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Woohoo");
+
+
+		int number = ran.nextInt(4) + 0;
+			
+			 
+			 if (number == 0) {
+					JOptionPane.showMessageDialog(null, "You will buy a new plant soon ");
+				} else if (number == 1) {
+					JOptionPane.showMessageDialog(null, "You will be betrayed by someone close");
+				} else if (number == 2) {
+					JOptionPane.showMessageDialog(null, "You will have a good day very soon...");
+				} else {
+					JOptionPane.showMessageDialog(null, "You will discover somthing special!");
+				}	
 
 	 }
+	
 	 
 } //Public class end DONT REMOVE
 
---
+++ src/_03_gui_from_scratch/_2_jack_in_the_box/jackInTheBox.java
@@ -0,0 +1,11 @@
+package _03_gui_from_scratch._2_jack_in_the_box;
+
+public class jackInTheBox {
+
+	public void showButton() {
+        System.out.println("Button clicked");
+   }
+	
+	
+	
+}
